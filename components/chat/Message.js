2d2b2ee09f1534c508cf8867b175ca562b212b89
Kyrylo Kapinos <kapinos.kirill.99@gmail.com>
2025-07-15 14:11:55 +0200

    Added if message was edited info

--- components/chat/Message.js
+++ components/chat/Message.js
@@ -7,14 +7,15 @@ function Message({message, setInput,  setChangingMessage,  setId, setIsChanging,
 
 
     return(
-        <div onClick={() => {
+            <div onClick={() => {
             if(localStorage.getItem("username") === message.sender){
                 setMessageMenu(prev => !prev);
-            }}}  className={`flex flex-col ${localStorage.getItem("username") === message.sender ? 'items-end' : 'items-start'} relative  inline-block`}>
+            }}}  className={`flex flex-col  relative  inline-block ${localStorage.getItem("username") === message.sender ? 'items-end' : 'items-start'}`}>
 
             {messageMenu && <MessageMenu selectedChatId={selectedChatId}  setIsChanging={setIsChanging} stompClient={stompClient}
-                            setId={setId} id={message.id} setChangingMessage={setChangingMessage} message={message} messageType={message.type}/>}
-
+                            setId={setId}  setChangingMessage={setChangingMessage} message={message} />}
+            
+            
             <li className={`max-w-[50%] ${localStorage.getItem("username") === message.sender ? "bg-zinc-800 rounded-l-xl  " : "bg-slate-600 rounded-r-xl"}
                      text-white p-3 w-fit  rounded-t-xl break-all`}>
                 {message.type === "text" ? 
@@ -23,28 +24,29 @@ function Message({message, setInput,  setChangingMessage,  setId, setIsChanging,
                 }
              </li>
              <div className="text-white text-sm flex items-center">
+                
                 <p className="pr-2">
                     {message.time}
-                 </p>
+                </p>
                  
-                 {message.seen ?  <p>
+                {message.seen ?  <p>
                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor"  viewBox="0 0 16 16">
                         <path d="M12.354 4.354a.5.5 0 0 0-.708-.708L5 10.293 1.854 7.146a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0
                          .708 0zm-4.208 7-.896-.897.707-.707.543.543 6.646-6.647a.5.5 0 0 1 .708.708l-7 7a.5.5 0 0 1-.708 0"/>
                         <path d="m5.354 7.146.896.897-.707.707-.897-.896a.5.5 0 1 1 .708-.708"/>
                     </svg>
-                 </p> 
-                 :
-                 <p>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
-                    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>
-                    </svg>
-                 </p>
-                 
-                 }
-                 
+                </p> 
+                :
+                <p>
+                   <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
+                   <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>
+                   </svg>
+                </p>
+                }
+                {message.edited && <p>Edited</p>}
              </div>
         </div>
+        
     );
 }
 
--- components/chat/MessageMenu.js
+++ components/chat/MessageMenu.js
@@ -1,21 +1,21 @@
 import { deleteMessageSocket } from "../../services/messageService";
 
 
-export default function MessageMenu({setChangingMessage, messageType, message, id, setId, setIsChanging, selectedChatId, stompClient}){
+export default function MessageMenu({setChangingMessage,  message,  setId, setIsChanging, selectedChatId, stompClient}){
 
 
     return(
         <div className="absolute text-white bg-[gray] rounded-lg bottom-full z-50">
             <p className="hover:bg-[#3f3f3f] duration-200 p-2 rounded-t-lg cursor-pointer" 
                 onClick={() => {
-                    deleteMessageSocket(selectedChatId, id, stompClient);
+                    deleteMessageSocket(selectedChatId, message.id, stompClient);
                 }}> Delete</p>
 
             <p className="hover:bg-[#3f3f3f] duration-200 p-2 rounded-b-lg cursor-pointer" 
                 onClick={() => {
-                    if(messageType === 'text'){
-                      setChangingMessage(message);
-                      setId(id);
+                    if(message.type === 'text'){
+                      setChangingMessage(message.message);
+                      setId(message.id);
                       setIsChanging(true);
                     }}}>Change</p>
         </div>
